db0217b14906ae0e3fe0ec91cc25055ed045d5c7
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-04-28 21:45:02 -0400

    Repair testing and working

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,29 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair, get } = require('./enhancer.js');
 // test away!
+
+describe('enhancer.js', () => {
+    describe('enhancer', () => {
+
+        describe('enhancer.repair()', () => {
+            it('should accept an item object and return a new item with the durability restored to 100.', () => {
+                expect(repair({ name: 'Canopus Lux', durability: 10, enhancement: 2 })).toEqual({
+                    name: 'Canopus Lux',
+                    durability: 100,
+                    enhancement: 2
+                });
+                expect(repair({ name: 'Anabasis Lux', durability: 99, enhancement: 5 })).toEqual({
+                    name: 'Anabasis Lux',
+                    durability: 100,
+                    enhancement: 5
+                });
+                expect(repair({ name: 'Mimesis Lux', durability: 69, enhancement: 12 })).toEqual({
+                    name: 'Mimesis Lux',
+                    durability: 100,
+                    enhancement: 12
+                });
+            });
+        });
+
+
+    });
+});
